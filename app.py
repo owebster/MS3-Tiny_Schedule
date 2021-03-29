1c4e5300ab1b4c05cada516d5bac6bdc65e0af47
Owain Webster <owainpw@gmail.com>
2021-03-29 22:35:18 +0100

    add session.clear to replace session.pop for logout function

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def message_board():
 def logout():
     # remove user from session cookie
     flash("You have been logged out")
-    session.pop("user")
+    session.clear()
     return redirect(url_for("login_page"))
 
 
