7e3bcce908c674d8c0d63646cd4d9fbfd88f9345
Owain Webster <owainpw@gmail.com>
2021-04-08 18:53:19 +0100

    sync up emailjs to contact.html

--- static/js/emailjs.js
+++ static/js/emailjs.js
@@ -1,3 +1,15 @@
-(function () {
-    emailjs.init("user_iqJAXGFrjNNsx0GoZBdyh");
-})();
+function sendMail(contactForm){
+    emailjs.send("service_9sp6wie", "template_p4l7sii", {
+        "from_name": contactForm.from_name.value,
+        "from_email": contactForm.from_email.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("success", response);
+        },
+        function(error) {
+            console.log("fail", error);
+        });
+    return false;
+}
